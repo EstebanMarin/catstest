91888a79b1ef7e343bfb0dabfec499407c75c15f
esteban <esteban.marin@adidas.com>
2021-09-24 20:58:07 -0500

    implicist!

--- src/main/scala/com/estebanmarin/catstest/Main.scala
+++ src/main/scala/com/estebanmarin/catstest/Main.scala
@@ -1,8 +1,6 @@
 package com.estebanmarin
 package catstest
 
-import scala.util.Try
-
 object Main extends App {
   println("─" * 100)
   // implicit classes
@@ -16,6 +14,29 @@ object Main extends App {
   }
 
   val greeting = "Petter".greet
+//importing implicits conversions in Scope
+  import scala.concurrent.duration._
+  val oneSet = 1.second
+  println(oneSet)
+  //implicit arguments and values
+  def increment(x: Int)(implicit amount: Int) = x + amount
+  implicit val amount = 4
+  println(increment(4))
 
+  trait JSONSerializer[T] {
+    def toJson(value: T): String
+  }
+
+  def listToJson[T](list: List[T])(implicit serializer: JSONSerializer[T]): String =
+    list.map(value => serializer.toJson(value)).mkString("[", ",", "]")
+
+  implicit val personSerializer: JSONSerializer[Person] = new JSONSerializer[Person] {
+    def toJson(person: Person): String = person.name
+  }
+
+  val personJson = listToJson(List(Person("Alice"), Person("Bob")))
+  println(personJson)
+
+  //implicit argument is used to PROVE THE EXISTENCE of a type
   println("─" * 100)
 }
