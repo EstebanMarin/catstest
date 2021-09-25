4d0c28f34ba6117945cdb7758a25fd41480611fb
esteban <esteban.marin@adidas.com>
2021-09-24 20:24:22 -0500

    checking implicits

--- src/main/scala/com/estebanmarin/catstest/Main.scala
+++ src/main/scala/com/estebanmarin/catstest/Main.scala
@@ -1,10 +1,21 @@
 package com.estebanmarin
 package catstest
 
+import scala.util.Try
+
 object Main extends App {
   println("─" * 100)
+  // implicit classes
 
-  println("hello world")
+  case class Person(name: String) {
+    def greet: String = s"Hi, My name is $name"
+  }
+  implicit class ImpersonableString(name: String) {
+    println("Hello in the implicit")
+    def greet: String = s"Hello $name"
+  }
+
+  val greeting = "Petter".greet
 
   println("─" * 100)
 }
