4094f472dea320c70c7264ed5dd4b6b5d986cb45
esteban <esteban.marin@adidas.com>
2021-09-26 17:57:42 -0500

    more on monadsa

--- src/main/scala/com/estebanmarin/catstest/DataStructures/Monads.scala
+++ src/main/scala/com/estebanmarin/catstest/DataStructures/Monads.scala
@@ -39,9 +39,37 @@ object MyMonads {
     c <- charFuture
   } yield (n, c)
 
+  /*
+  Pattern
+  - wrappting a value into a Monadic value
+  - the flatMap mechanism
+  MONAD
+   */
+
+  trait MyMonad[M[_]] {
+    def pure[A](value: A): M[A]
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+  }
+
+  import cats.Monad
+  import cats.instances.option._ // implicit Monad[Option]
+  val optionMonad = Monad[Option]
+
+  val anOption: Option[Int] = optionMonad.pure(4) // Some (4)
+
+//   val anOptionTransformed = aTransformedOption(anOption)
+  val aTransformedOption: Option[Int] =
+    optionMonad.flatMap(anOption)(x => if (x % 3 == 0) Some(x + 1) else None)
+
+  import cats.instances.list._
+  val listMonads: Monad[List] = Monad[List]
+  val aList: List[Int] = listMonads.pure(3)
+  val aTransformedList: List[Int] = listMonads.flatMap(aList)(x => List(x + 1))
+
   def main(args: Array[String]): Unit = {
     println("-" * 50)
     println("In Monads")
+    println(this.aList)
     println("-" * 50)
   }
 }
