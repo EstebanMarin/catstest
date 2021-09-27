a9583545153d574a962273ce283b58c10eea9cab
esteban <esteban.marin@adidas.com>
2021-09-26 20:58:36 -0500

    compiling

--- src/main/scala/com/estebanmarin/catstest/DataStructures/UsingMonads.scala
+++ src/main/scala/com/estebanmarin/catstest/DataStructures/UsingMonads.scala
@@ -8,9 +8,6 @@ object UsingMonads {
   import cats.instances.list._
   val monadList: Monad[List] = Monad[List]
   val aSimpleList: List[Int] = monadList.pure(2)
-  import cats.syntax.applicative._
-  import cats.syntax.functor._
-  import cats.syntax.flatMap._
 
   //below applicative to a types
   val anExtendedList: List[Int] = for {
@@ -42,10 +39,71 @@ object UsingMonads {
     else Right("Amsterdam, NL")
 
   //say that you want to get the the order status and the track the location of the package
+  val orderID = 4567
+
+  def orderLocation(orderId: Long): LoadingOr[String] =
+    loadingMonad.flatMap(getOrderStatus(orderId))(orderStatus => trackLocation(orderStatus))
+
+  //use the extension methods
+  import cats.syntax.applicative._
+  import cats.syntax.functor._
+  import cats.syntax.flatMap._
+  val orderLocationBetter: LoadingOr[String] = for {
+    status <- getOrderStatus(orderID)
+    location <- trackLocation(status)
+  } yield location
+
+  //TODO: Service layer API of a web APP
+  case class Connection(host: String, port: String)
+  type Port = String
+  type Host = String
+  type Config = Map[Host, Port]
+  val conf: Config = Map(
+    "host" -> "localhost",
+    "port" -> "4040",
+  )
+
+  trait HttpService[M[_]] {
+    def getConnection(cif: Config): M[Connection]
+    def issueRequest(connection: Connection, payload: String): M[String]
+  }
+  /*
+  TODO provide a real implementatio of HTTP service using either a TRy Option Future Either
+   */
+  object OptionHttpService extends HttpService[Option] {
+    override def getConnection(cif: Config): Option[Connection] = for {
+      h <- conf.get("host")
+      p <- conf.get("port")
+    } yield Connection(h, p)
+
+    override def issueRequest(connection: Connection, payload: String): Option[String] =
+      if (payload.length > 50) None else Some(s"Request $payload has been accpeted")
+  }
+
+  // TODO implement another HTTPServive with LoadingOr or ErrorOr
+  object LoadingOrHttpService extends HttpService[LoadingOr] {
+    override def getConnection(cif: Config): LoadingOr[Connection] =
+      if (!cif.contains("host") || !cif.contains("port"))
+        Left("Error Connexion")
+      else
+        Right(Connection(conf("host"), conf("port")))
+    override def issueRequest(connection: Connection, payload: String): LoadingOr[String] =
+      if (payload.length > 50) Left("Paila") else Right(s"Request $payload has been accpeted")
+  }
 
   def main(args: Array[String]): Unit = {
     println("-" * 50)
-    // println(this.anEither)
+    val response: Option[String] = OptionHttpService
+      .getConnection(conf)
+      .flatMap(connexion => OptionHttpService.issueRequest(connexion, "Hello Esteban"))
+
+    val secondResponse: Option[String] = for {
+      conn: Connection <- OptionHttpService.getConnection(conf)
+      response: String <- OptionHttpService.issueRequest(conn, "Hello Second Time")
+    } yield response
+
+    println(response)
+    println(secondResponse)
     println("-" * 50)
   }
 }
